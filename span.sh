3afa4a827fdcd05c6ae7da71db421a07810da570
Aiden Gindin <aiden@aidengindin.com>
2026-07-22 09:45:54 -0400

    refactor: rename flash script

--- span.sh
+++ span.sh
@@ -2,8 +2,8 @@ export QMK_USERSPACE=~/code/qmk_userspace/
 
 cmd='make holykeebs/spankbd:aidengindin:flash -e USER_NAME=holykeebs -e POINTING_DEVICE=trackpoint_trackball -e CONSOLE=yes -e CC=arm-none-eabi-gcc -e OBJCOPY=arm-none-eabi-objcopy -j8'
 
-echo $HOME
-pushd $HOME/code/qmk_firmware/
+echo "$HOME:"
+pushd "$HOME/code/qmk_holykeebs/" || exit 1
 
 echo "Compiling left side firmware. Plug in left side. QK_BOOT (spc + p) to enter bootloader."
 $cmd -e SIDE=left || exit 1
